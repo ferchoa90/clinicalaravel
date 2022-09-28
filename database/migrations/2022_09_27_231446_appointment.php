42014f3eb9aca603beaad96eead8dc515a473af1
Mario Aguilar <marioaguilar1990@gmail.com>
2022-09-27 19:23:35 -0500

    Init Prueba
    Init Prueba

---
+++ database/migrations/2022_09_27_231446_appointment.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('dateapp');
+            $table->longText('note');
+            $table->unsignedInteger('doctor_id');
+            $table->tinyInteger('statusapp')->default(1);
+            $table->tinyInteger('status')->default(1);
+            $table->unsignedInteger('user_cre');
+            $table->unsignedInteger('user_upd')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
